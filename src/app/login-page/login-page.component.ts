2d8e6a2254e0a13ab3485a2b0ecb175409950a3a
irenevt2 <86534113+irenevt2@users.noreply.github.com>
2022-10-14 19:35:50 -0300

    routing de los componentes y acceso al componente administradoa traves del login e implementando modal

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  // event: any;
+  constructor(
+    private router:Router,
+    private modal:NgbModal
+  ) { }
 
   ngOnInit(): void {
   }
 
+  iniciarSesion(){
+    let ref = this.modal.open(LoginComponent)
+    ref.componentInstance.isModal = true;
+    ref.componentInstance.evento.subscribe((params:any)=>{
+      ref.close();
+      this.evaluar(params)
+    })
+  }
+
+  evaluar(params:any){
+    console.log(params)
+    if(params.event == "inicio sesion"){
+      this.router.navigate(["administrador"])
+    }
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Optional, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -8,24 +9,48 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
 
+  @Input() isModal = false;
+  @Output() evento = new EventEmitter();
+
   username = "";
   password = "";
 
   formlogin : FormGroup = new FormGroup({
     username : new FormControl('', [Validators.required]),
-    password : new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password : new FormControl('', [Validators.required, Validators.minLength(4)]),
   });
 
-  constructor( private formBuilder:FormBuilder ) { }
+  constructor( 
+    @Optional() private modalActivo:NgbActiveModal 
+    ) { }
 
   ngOnInit(): void {
   }
   validar(){
-    console.log(this.formlogin)
+    // console.log(this.formlogin)
     if(this.formlogin.valid){
+      this.username = this.formlogin.value.username;
+      this.password = this.formlogin.value.password;
       console.log(this.formlogin.value)
 
+      if(this.username=="root" && this.password=="root"){
+        alert("Inicio de sesion");
+        this.evento.emit({
+          event:'inicio sesion',
+          user: {
+
+          }
+        })
+      }else{
+        alert("Error en los datos")
+      }
+    }else{
+      alert('revisar datos')
     }
+  }
+
+  cerrar(){
+    this.modalActivo.close();
   }
 
 }
